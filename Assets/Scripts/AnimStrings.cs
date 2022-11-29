d5a0947d73f200efa37df47d830ff53c01a56191
Flo-Roeder <flowrain@gmx.de>
2022-11-29 16:11:07 +0100

    first controlls and animations

---
+++ Assets/Scripts/AnimStrings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+internal class AnimStrings
+{
+    internal static string yVelocity = "yVelocity";
+    internal static string xVelocity = "xVelocity";
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    Animator anim;
+
+    private Vector2 moveInput;
+    public float moveSpeed;
+
+    private void Awake()
+    {
+        rb= GetComponent<Rigidbody2D>();
+        anim= GetComponent<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +28,19 @@ public class PlayerController : MonoBehaviour
     {
         
     }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x*moveSpeed, moveInput.y*moveSpeed);
+        anim.SetFloat(AnimStrings.xVelocity, rb.velocity.x);
+        anim.SetFloat(AnimStrings.yVelocity, rb.velocity.y);
+    }
+
+    public void Movement(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+
+    }
+
 }
