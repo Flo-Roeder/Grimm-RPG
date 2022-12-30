73e21bbe5c61d7332bd537abefdf9fb6d3a855f8
Flo-Roeder <109297588+Flo-Roeder@users.noreply.github.com>
2022-12-30 13:04:41 +0100

    test stuff: reload run, quit, pause, slowmo, cam zoom

--- Assets/Scripts/Manager/GameController.cs
+++ Assets/Scripts/Manager/GameController.cs
@@ -1,12 +1,15 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
+using UnityEditor.Rendering.LookDev;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] int tempCounter;
+    [SerializeField] CinemachineVirtualCamera vCam;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +30,27 @@ public class GameController : MonoBehaviour
     public void TimeChange() 
     {
         Time.timeScale = Time.timeScale == 1 ? .5f : 1;
+    }
+
+    public void ReloadRun(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public void QuitToDesktop()
+    {
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+                Application.Quit();
+    }
+
+    public void Zoom(InputAction.CallbackContext context)
+    {
+        float tempValue=context.ReadValue<float>();
+        vCam.m_Lens.OrthographicSize = Mathf.Clamp(vCam.m_Lens.OrthographicSize+tempValue,10f,20f);
     }
 }
