de5096740fb846041017ecc0b8fa9041ab7fdbb5
Flo-Roeder <109297588+Flo-Roeder@users.noreply.github.com>
2023-01-18 11:45:26 +0100

    storm+freeze ability

--- Assets/Character/Player/Abilities/BlockAbility.cs
+++ Assets/Character/Player/Abilities/BlockAbility.cs
@@ -9,6 +9,7 @@ public class BlockAbility : Ability
     [SerializeField] GameObject blockGameobject;
     GameObject blockInstance;
     [SerializeField] int pushAmount;
+    [SerializeField] int radius;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,6 +34,7 @@ public class BlockAbility : Ability
             playerStats.currentStamina -= staminaCost;
             //player.healthController.TakeStamina(staminaCost, false);
             blockInstance = Instantiate(blockGameobject,parent.transform);
+            blockInstance.transform.localScale=new Vector3(radius, radius, 0);
             blockInstance.GetComponent<BlockPush>().knockbackTime = activeTime;
             blockInstance.GetComponent<BlockPush>().knockbackAmount= pushAmount;
         }
